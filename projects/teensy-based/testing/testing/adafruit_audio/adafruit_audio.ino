3967b77c0b433ebb955feb4aad89b5e0bc6163a9
Justin Mathews <github@justinmathews.ca>
2023-05-22 23:11:32 -0400

    testing

---
+++ projects/teensy-based/testing/testing/adafruit_audio/adafruit_audio.ino
@@ -0,0 +1,61 @@
+#include <effect_fade.h>
+#include <analyze_fft1024.h>
+#include <memcpy_audio.h>
+#include <output_i2s.h>
+#include <synth_waveform.h>
+#include <play_sd_raw.h>
+#include <control_cs42448.h>
+#include <synth_dc.h>
+#include <effect_flange.h>
+#include <filter_variable.h>
+#include <play_memory.h>
+#include <control_ak4558.h>
+#include <effect_delay.h>
+#include <analyze_fft256.h>
+#include <effect_delay_ext.h>
+#include <analyze_peak.h>
+#include <analyze_tonedetect.h>
+#include <filter_fir.h>
+#include <effect_bitcrusher.h>
+#include <synth_pwm.h>
+#include <control_sgtl5000.h>
+#include <synth_pinknoise.h>
+#include <effect_waveshaper.h>
+#include <AudioControl.h>
+#include <Audio.h>
+#include <output_dacs.h>
+#include <synth_simple_drum.h>
+#include <synth_karplusstrong.h>
+#include <effect_envelope.h>
+#include <synth_tonesweep.h>
+#include <spi_interrupt.h>
+#include <effect_reverb.h>
+#include <play_queue.h>
+#include <AudioStream.h>
+#include <analyze_notefreq.h>
+#include <mixer.h>
+#include <input_adcs.h>
+#include <play_fs_wav.h>
+#include <filter_biquad.h>
+#include <control_cs4272.h>
+#include <effect_chorus.h>
+#include <synth_whitenoise.h>
+#include <control_wm8731.h>
+#include <record_queue.h>
+#include <analyze_print.h>
+#include <effect_midside.h>
+#include <input_i2s.h>
+#include <synth_sine.h>
+#include <analyze_rms.h>
+#include <effect_multiply.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
